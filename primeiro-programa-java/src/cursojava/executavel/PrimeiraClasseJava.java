c8ba42e25a8679f452d54c46bd46898b76083ec9
Caua Santana <santanacaua155@gmail.com>
2022-08-16 18:28:42 -0300

    testando nova forma de comitar, com token

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -3,6 +3,6 @@ package cursojava.executavel;
 public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
-		System.out.println("Meu primeiro programa em Java");
+		System.out.println("Meu primeiro programa em Java, caua insano");
 	}
 }
