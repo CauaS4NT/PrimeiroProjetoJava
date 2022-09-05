d275786a4748920f032a28a226b427649baf5d97
Caua Santana <santanacaua155@gmail.com>
2022-09-05 17:29:08 -0300

    Calculo da media das notas

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -7,9 +7,10 @@ public class PrimeiraClasseJava {
 		
 		int nota1 = 90;
 		int nota2 = 70;
-		int nota3 = 51;
+		int nota3 = 53;
 		int nota4 = 88;
-
+ 
+		//Calculando a media das notas
 	System.out.println("A media das notas é = " + (nota1 + nota2 + nota3 + nota4) / 4);
 		
 	}
