a8d2efcf6fe1573a9e77d3013bb28914f5d69911
Caua Santana <santanacaua155@gmail.com>
2022-09-05 17:23:50 -0300

    calculando a media das notas

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,7 +2,15 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
+	//metodo main é um metodo auto executavel no java
 	public static void main(String[] args) {
-		System.out.println("Meu primeiro programa em Java, caua insano");
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 51;
+		int nota4 = 88;
+
+	System.out.println("A media das notas é = " + (nota1 + nota2 + nota3 + nota4) / 4);
+		
 	}
 }
